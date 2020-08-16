76b4496435aace03de99455409bfd016b7739e39
jb12mbh2 <69480495+jb12mbh2@users.noreply.github.com>
2020-08-16 00:51:21 -0300

    Update myVM.sh

--- myVM.sh
+++ myVM.sh
@@ -84,7 +84,7 @@ for assinatura in "${subscription[@]}"
          cmd="$cmd"
          #echo $cmd
          
-         az vm extension set --publisher Microsoft.Azure.Extensions --version 2.0 --name CustomScript --vm-name $nome --resource-group $RG --no-wait --settings '{"fileUris": ["https://raw.githubusercontent.com/jb12mbh2/tools/master/vm.sh"],"commandToExecute":"sh vm.sh VM_$regiao "}'
+         az vm extension set --publisher Microsoft.Azure.Extensions --version 2.0 --name CustomScript --vm-name $nome --resource-group $RG --no-wait --settings '{"fileUris": ["https://raw.githubusercontent.com/jb12mbh2/tools/master/vm.sh"],"commandToExecute":"sh vm.sh VM "}'
          echo
      done
      
