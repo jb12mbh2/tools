bd9331c1507dde07799c87b56d128cec0abdaff9
jb12mbh2 <69480495+jb12mbh2@users.noreply.github.com>
2020-08-16 00:46:21 -0300

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 wget https://raw.githubusercontent.com/jb12mbh2/tools/master/create.sh
-wgte https://raw.githubusercontent.com/jb12mbh2/tools/master/Pool.json
+wget https://raw.githubusercontent.com/jb12mbh2/tools/master/Pool.json
 chmod u+x create.sh
 ./create.sh
